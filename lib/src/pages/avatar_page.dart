1ed23e2779657f1b8ce851820c18eae66188c4a2
Eli Alejandro <iscelialejandro@gmail.com>
2019-08-02 13:31:02 -0500

    Avarts

--- lib/src/pages/avatar_page.dart
+++ lib/src/pages/avatar_page.dart
@@ -8,7 +8,25 @@ class AvatarPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Avatar Page'),
+        actions: <Widget>[
+          Container(
+            padding: EdgeInsets.all(8.0),
+            child: CircleAvatar(
+              backgroundColor: Colors.amber,
+              backgroundImage: NetworkImage('http://culto.latercera.com/wp-content/uploads/2018/09/Gps3Oca-900x600.jpg'),
+              radius: 20.0,
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.only(right: 10.0),
+            child: CircleAvatar(
+              backgroundColor: Colors.amber,
+              child: Text('DH'),
+            ),
+          )
+        ],
       ),
+
     );
   }
 }
