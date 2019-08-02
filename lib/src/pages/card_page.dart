d15d25536286dd559089c82559e11da937184049
Eli Alejandro <iscelialejandro@gmail.com>
2019-08-02 13:50:01 -0500

    Tarjeta uno

--- lib/src/pages/card_page.dart
+++ lib/src/pages/card_page.dart
@@ -9,6 +9,28 @@ class CardPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Card page'),
       ),
+      body: ListView(
+        children: <Widget>[
+          _crearTarjetaUno(),
+        ],
+      ),
+    );
+  }
+
+  _crearTarjetaUno() {
+    return Card(
+      elevation: 10.0,
+      child: Column(
+        children: <Widget>[
+          ListTile(
+            leading: Icon ( 
+              Icons.photo_album, 
+              color: Colors.purple, ),
+            title: Text('Tarjeta uno'),
+            subtitle: Text('Soy un subtitulo'),
+          ),
+        ],
+      ),
     );
   }
 }
